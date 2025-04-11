7f4caa0899be2b168350cb47ff7f4f7e88e40c14
Veer1578 <sweetyatulshah@gmail.com>
2025-04-11 09:13:38 +0530

    logicalOperators

---
+++ logicalOperators.py
@@ -0,0 +1,10 @@
+a = 23
+b = 24
+c = 22
+
+if a > b or a < c :
+    print("I am using OR operator.")
+elif a < b and a > c :
+    print("I am using AND operator.")
+else:
+    print("Both operators are incorrect.")
